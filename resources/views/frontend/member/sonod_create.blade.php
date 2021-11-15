fc342d8a014d1cd75048865c74f8933c6e2bea5a
Mazharul Islam <mazharulslm9@gmail.com>
2021-11-16 02:24:36 +0600

    member design sonod

--- resources/views/frontend/member/sonod_create.blade.php
+++ resources/views/frontend/member/sonod_create.blade.php
@@ -17,7 +17,7 @@
                     <div class="form-row">
                         <div class="col-md-6">
                             <div class="mb-3">
-                                <label for="">নাম</label>
+                                <label for=""> @if ($id == 5) মৃত ব্যক্তির @endif নাম</label>
                                 <input name="name" required type="text" class="form-control" placeholder="সম্পূর্ণ নাম">
                             </div>
                         </div>
@@ -148,6 +148,32 @@
                         </div>
 
                     </div>
+                    @if ($id == 5)
+                    <div class="form-row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                           <table class="table" style="background: green">
+            <thead>
+              <tr>
+                <th style="color: white;">উত্তরাধীকারীগনের নাম</th> 
+                <th style="color: white;">সম্পর্ক</th>
+                 <th style="color: white;">NID</th>
+                 <th style="color: white;">মন্তব্য</th>
+                 <th><button type="button" style="padding: 3px; color: white;" class="btn btn-warning btn-sm add_more"><i class="fa fa-plus"></i></button></th>
+              </tr>
+                
+            </thead>
+            
+            <tbody>
+                  
+            </tbody>
+           
+          </table>
+                            </div>
+                        </div>
+
+                    </div>
+                    @endif
                     <div class="text-right">
                         <button type="submit" class="btn btn-primary">সাবমিট করুন</button>
                     </div>
@@ -158,6 +184,101 @@
                         </div>
                     </div>
                 </div>
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+ $(document).ready(function(){
+    $(document).on('change', '.guardian_status', function(){
+        var guardian_status = $('.guardian_status').val();
+
+        
+        if(guardian_status == 'father'){
+            $('.guardian_val').attr('placeholder', 'পিতার নাম');
+             $('.guardian_val').attr('name', 'father_name');
+        }
+        else{
+            $('.guardian_val').attr('placeholder', 'স্বামীর নাম');
+            $('.guardian_val').attr('name', 'husband_name');
+        }
+        
+    });
+
+    $(document).on('change', '.birth_nid', function(){
+        var birth_nid = $('.birth_nid').val();
+
+        
+        if(birth_nid == 'select_birth'){
+            $('.val_birth_nid').attr('placeholder', 'জন্ম নিবন্ধন নম্বর');
+             $('.val_birth_nid').attr('name', 'birth_certificate');
+        }
+        else{
+            $('.val_birth_nid').attr('placeholder', 'এনআইডি নম্বর');
+             $('.val_birth_nid').attr('name', 'nid');
+        }
+        
+    });
+    
+   $(document).on('click', '.add_more', function(e){
+       e.preventDefault();
+       Tbody();
+   });
+   
+   $(document).on("click", ".remove", function(e){
+       e.preventDefault();
+       
+      
+         $(this).parent().parent().remove();
+        
+      
+    });
+    
+    $(document).on('input', '.warsh_check', function(){
+        $('.check').val('1')
+    });
+ });
+ 
+ 
+ function Tbody()
+   {
+      var tr = 
+
+         '<tr>'+
+         '<td><input  type="text" name="warish_member_name[]" class="form-control warsh_check"></td>'+ 
+         
+         '<td><select name="relation[]" class="form-control"><option value="" selected="" disabled>সম্পর্ক</option><?php $rel = DB::table('relations')->get(); ?>@foreach($rel as $row)<option value="{{$row->relation_name}}">{{$row->relation_name}}</option>@endforeach</select></td>'+
+         
+          '<td><input type="text" name="member_nid[]" class="form-control"></td>'+
+        
+        '<td><input  type="text" name="comment[]" class="form-control"></td>'+
+
+        
+        
+
+         '<td><a style="cursor: pointer;" class="btn btn-danger btn-sm remove">X</a></td>'+
+
+         
+       '</tr>';
+
+       
+
+       $('tbody').append(tr);
+   }
+
+
+ function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+             $("#image").css("display", "block");
+              $('#image')
+                  .attr('src', e.target.result)
+                  .width(80)
+                  .height(80);
+          };
+          reader.readAsDataURL(input.files[0]);
+      }
+   }
+</script>
 @endsection
 
 
